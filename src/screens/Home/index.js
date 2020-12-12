82a2e27114d6014f1097bef85df725443f6e1e60
StephanJ98 <diego.jeandon@gmail.com>
2020-12-13 00:43:06 +0100

    :bug: Visual Bug Solved

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -6,22 +6,20 @@ import Fontisto from 'react-native-vector-icons/Fontisto'
 export default function HomeScreen() {
     return (
         <View>
+            <Pressable
+                style={styles.searchButton}
+                onPress={() => navigation.navigate('Destination Search')}>
+                <Fontisto name="search" size={25} color={'#f15454'} />
+                <Text style={styles.buttonText}>Where are you going?</Text>
+            </Pressable>
             <ImageBackground
                 source={require('../../../assets/images/wallpaper.jpg')}
-                style={styles.image}
-            >
-                <Pressable
-                    style={styles.searchButton}
-                    onPress={() => console.warn('Search Button Clicked')}
-                >
-                    <Fontisto name='search' size={25} color={'#f15454'} />
-                    <Text style={styles.searchButtonText}>Where are you going?</Text>
-                </Pressable>
+                style={styles.image}>
                 <Text style={styles.title}>Go Near</Text>
+
                 <Pressable
                     style={styles.button}
-                    onPress={() => console.warn('Explore Button Clicked')}
-                >
+                    onPress={() => console.warn('Explore Btn clicked')}>
                     <Text style={styles.buttonText}>Explore nearby stays</Text>
                 </Pressable>
             </ImageBackground>
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,50 +1,46 @@
-import { StyleSheet, Dimensions } from 'react-native'
+import { StyleSheet, Dimensions } from 'react-native';
 
 const styles = StyleSheet.create({
-    searchButton: {
-        backgroundColor: 'white',
-        width: Dimensions.get('screen').width - 20,
-        height: 60,
-        borderRadius: 30,
-        marginHorizontal: 10,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        position: 'absolute',
-        top: 20,
-        zIndex: 100
-    },
-    searchButtonText: {
-        fontSize: 16,
-        fontWeight: 'bold'
-    },
     image: {
         width: '100%',
         height: 500,
         resizeMode: 'cover',
-        justifyContent: 'center'
+        justifyContent: 'center',
     },
     title: {
         fontSize: 80,
         fontWeight: 'bold',
         color: 'white',
         width: '70%',
-        marginLeft: 25
+        marginLeft: 25,
     },
     button: {
-        backgroundColor: 'white',
+        backgroundColor: '#fff',
         width: 200,
-        marginLeft: 25,
-        marginTop: 25,
         height: 40,
         borderRadius: 10,
+        marginTop: 25,
+        marginLeft: 25,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
     },
     buttonText: {
         fontSize: 16,
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+    },
+    searchButton: {
+        backgroundColor: '#fff',
+        height: 60,
+        width: Dimensions.get('screen').width - 20,
+        borderRadius: 30,
+        marginHorizontal: 10,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        top: 50,
+        zIndex: 100,
     }
-})
+});
 
-export default styles
+export default styles;
