5daf7ab6c4f7bc13ca0e600b19f6b8a37c717556
StephanJ98 <diego.jeandon@gmail.com>
2020-12-14 17:40:03 +0100

    Guests

--- src/screens/Guests/index.js
+++ src/screens/Guests/index.js
@@ -1,11 +1,78 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, Pressable } from 'react-native'
 import styles from './styles'
 
 export default function GuestsScreen() {
+
+    const [adults, setAdults] = useState(0)
+    const [children, setChildren] = useState(0)
+    const [infants, setInfants] = useState(0)
+
     return (
         <View>
-            <Text>aaaaaaaa</Text>
+            <View style={styles.row}>
+                <View>
+                    <Text style={styles.tag}>Adults</Text>
+                    <Text style={styles.subtag}>Ages 13 or above</Text>
+                </View>
+                <View style={styles.buttonsContainer}>
+                    <Pressable
+                        onPress={() => setAdults(Math.max(0, adults - 1))}
+                        style={styles.button}
+                    >
+                        <Text style={styles.simbol}>-</Text>
+                    </Pressable>
+                    <Text style={styles.value}>{adults}</Text>
+                    <Pressable
+                        onPress={() => setAdults(adults + 1)}
+                        style={styles.button}
+                    >
+                        <Text style={styles.simbol}>+</Text>
+                    </Pressable>
+                </View>
+            </View>
+            <View style={styles.row}>
+                <View>
+                    <Text style={styles.tag}>Children</Text>
+                    <Text style={styles.subtag}>Ages 2-12</Text>
+                </View>
+                <View style={styles.buttonsContainer}>
+                    <Pressable
+                        onPress={() => setChildren(Math.max(0, children - 1))}
+                        style={styles.button}
+                    >
+                        <Text style={styles.simbol}>-</Text>
+                    </Pressable>
+                    <Text style={styles.value}>{children}</Text>
+                    <Pressable
+                        onPress={() => setChildren(children + 1)}
+                        style={styles.button}
+                    >
+                        <Text style={styles.simbol}>+</Text>
+                    </Pressable>
+                </View>
+            </View>
+            <View style={styles.row}>
+                <View>
+                    <Text style={styles.tag}>Infants</Text>
+                    <Text style={styles.subtag}>Under 2</Text>
+                </View>
+                <View style={styles.buttonsContainer}>
+                    <Pressable
+                        onPress={() => setInfants(Math.max(0, infants - 1))}
+                        style={styles.button}
+                    >
+                        <Text style={styles.simbol}>-</Text>
+                    </Pressable>
+                    <Text style={styles.value}>{infants}</Text>
+                    <Pressable
+                        onPress={() => setInfants(infants + 1)}
+                        style={styles.button}
+                    >
+                        <Text style={styles.simbol}>+</Text>
+                    </Pressable>
+                </View>
+            </View>
         </View>
     )
 }
--- src/screens/Guests/styles.js
+++ src/screens/Guests/styles.js
@@ -1,7 +1,41 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-    
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 20,
+        marginHorizontal: 20,
+        borderBottomWidth: 1,
+        borderColor: 'lightgrey'
+    },
+    tag: {
+        fontWeight: 'bold'
+    },
+    subtag: {
+        color: '#8d8d8d'
+    },
+    buttonsContainer: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    button: {
+        borderWidth: 1,
+        width: 30,
+        height: 30,
+        borderRadius: 15,
+        borderColor: '#676767',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    value: {
+        marginHorizontal: 20,
+        fontSize: 16
+    },
+    simbol: {
+        fontSize: 20,
+        color: '#474747'
+    }
 });
 
 export default styles;
